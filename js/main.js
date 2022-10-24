84e4e12211d9cf58bf418d6b2fe4ba6e73f0d0c0
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-10-24 16:05:51 +0200

    Elimina seconda condizione implementando la costante all'interno del ciclo

--- js/main.js
+++ js/main.js
@@ -4,42 +4,29 @@ const tableContainer = document.querySelector('.container');
 let calcolate;
 
 for(let i = 1; i <=100; i++){
+    const numberTable = document.createElement('div');
+    numberTable.classList.add('number-table');
 //creo condizione che imponga ai numeri multipli di 15 ( % 15 === 0 ) di stampare FizzBuzz    
     if( i % 15 == 0){
         calcolate = `FrizzBuzz`;
+        numberTable.classList.add('bg-green');
     }
 //creo condizione che imponga ai numeri multipli di 5 ( % 5 === 0 ) di stampare Buzz
     else if( i % 5 == 0){
-        calcolate = `Buzz` 
+        calcolate = `Buzz`;
+        numberTable.classList.add('bg-light-b');
     }
 //creo condizione che imponga ai numeri multipli di 3( % 3 === 0 ) di stampare Fizz
     else if(i % 3 == 0){
-        calcolate = `Frizz`       
+        calcolate = `Frizz`;
+        numberTable.classList.add('bg-yellow');      
     }
     else{
         calcolate = i;  
     }
 //console.log per vedere il flusso dei numeri
     console.log(calcolate);
-//BONUS
 
-    const numberTable = document.createElement('div');
-    numberTable.classList.add('number-table');
     numberTable.innerHTML = calcolate;
     tableContainer.append(numberTable);
-
-    if(calcolate === `FrizzBuzz`){
-        numberTable.classList.add('bg-green');
-    }
-    else if(calcolate === `Buzz`){
-        numberTable.classList.add('bg-light-b');
-    }
-    else if(calcolate === `Frizz`){
-        numberTable.classList.add('bg-yellow');
-    }
-    
 }
-
-
-
-
