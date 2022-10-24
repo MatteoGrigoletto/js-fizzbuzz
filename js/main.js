a55e8626866e7770522282c7a1ceed39ee13000a
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-10-24 13:46:56 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//creo ciclo for che stampi numeri compresi tra 1 a 100
+
+//creo condizione che imponga ai numeri multipli di 3( % 3 === 0 ) di stampare Fizz
+
+//creo condizione che imponga ai numeri multipli di 5 ( % 5 === 0 ) di stampare Buzz
+
+//creo condizione che imponga ai numeri multipli di 15 ( % 15 === 0 ) di stampare FizzBuzz
+
+//BONUS
+
+//
