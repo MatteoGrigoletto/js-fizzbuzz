8af3ac2f861975471fe5a0ceec471834a6513e47
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-10-24 14:55:03 +0200

    Aggiunge algoritmo per la gestione del flusso numerico e relativo print in console

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
+"use strict"
 //creo ciclo for che stampi numeri compresi tra 1 a 100
+let calcolate;
 
-//creo condizione che imponga ai numeri multipli di 3( % 3 === 0 ) di stampare Fizz
-
+for(let i = 1; i <=100; i++){
+//creo condizione che imponga ai numeri multipli di 15 ( % 15 === 0 ) di stampare FizzBuzz    
+    if( i % 15 == 0){
+        calcolate = `FrizzBuzz`;
+        document.querySelector('.ciao').innerHTML += calcolate; 
+    }
 //creo condizione che imponga ai numeri multipli di 5 ( % 5 === 0 ) di stampare Buzz
-
-//creo condizione che imponga ai numeri multipli di 15 ( % 15 === 0 ) di stampare FizzBuzz
+    else if( i % 5 == 0){
+        calcolate = `Buzz`
+        document.querySelector('.ciao').innerHTML += calcolate;    
+    }
+//creo condizione che imponga ai numeri multipli di 3( % 3 === 0 ) di stampare Fizz
+    else if(i % 3 == 0){
+        calcolate = `Frizz`
+        document.querySelector('.ciao').innerHTML += calcolate;      
+    }
+    else{
+        calcolate = i;
+        document.querySelector('.ciao').innerHTML += calcolate; 
+    }
+//console.log per vedere il flusso dei numeri
+    console.log(calcolate)
+}
 
 //BONUS
 
