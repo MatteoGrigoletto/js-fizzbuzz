863a62935cffe2625a2d4a5283ad57eb398ef03a
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-10-24 15:16:03 +0200

    Aggiunge append ed elimina le queryselector precdenti

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 "use strict"
 //creo ciclo for che stampi numeri compresi tra 1 a 100
+const tableContainer = document.querySelector('.container');
 let calcolate;
 
 for(let i = 1; i <=100; i++){
 //creo condizione che imponga ai numeri multipli di 15 ( % 15 === 0 ) di stampare FizzBuzz    
     if( i % 15 == 0){
         calcolate = `FrizzBuzz`;
-        document.querySelector('.ciao').innerHTML += calcolate; 
     }
 //creo condizione che imponga ai numeri multipli di 5 ( % 5 === 0 ) di stampare Buzz
     else if( i % 5 == 0){
-        calcolate = `Buzz`
-        document.querySelector('.ciao').innerHTML += calcolate;    
+        calcolate = `Buzz` 
     }
 //creo condizione che imponga ai numeri multipli di 3( % 3 === 0 ) di stampare Fizz
     else if(i % 3 == 0){
-        calcolate = `Frizz`
-        document.querySelector('.ciao').innerHTML += calcolate;      
+        calcolate = `Frizz`       
     }
     else{
-        calcolate = i;
-        document.querySelector('.ciao').innerHTML += calcolate; 
+        calcolate = i;  
     }
 //console.log per vedere il flusso dei numeri
-    console.log(calcolate)
+    console.log(calcolate);
+//BONUS
+    const numberTable = document.createElement('div');
+    numberTable.classList.add('number-table');
+    numberTable.innerHTML = calcolate;
+    tableContainer.append(numberTable);
 }
 
-//BONUS
 
-//
